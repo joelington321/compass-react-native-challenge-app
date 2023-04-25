13b0afe5742635dc484bd63edc9ca0fe12275f1e
joelington321 <joelington321@gmail.com>
2023-04-24 23:05:05 -0300

    test(Test a background color and font): test

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Font from 'expo-font';
 
 
 export default function App() {
+  
   return (
+    
     <View style={styles.container}>
-      <Text>First Setup envonvirment test</Text>
+      <Text style={styles.textTest}>WELCOME</Text>
+      <Text style={styles.textInput}>First Setup of the APP.</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +20,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2D2D2D',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textTest:{
+    color: '#D78f3C',
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'Khula'
+  },
+  textInput:{
+    color: '#656262'
+  }
 });
+
+
+async function loadFonts() {
+  await Font.loadAsync({
+    'Khula': require('../../assets/fonts/Khula-Regular.ttf'),
+  });
+}
