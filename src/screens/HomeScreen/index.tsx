0216382f1479fe01dd30d223203c6b34fbdad2d1
GabFerreira06 <gabi.ferreira3411@gmail.com>
2023-05-23 14:58:19 -0300

    *BUILD* Implemented the Product details screen and the navigation in the cards

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
 import styles from './style';
 
 
-const HomeScreen = () => {
+interface HomeScreenProp{
+    navigation: NavigationProp<ParamListBase>;
+};
+
+const HomeScreen = ( { navigation }: HomeScreenProp ) => {
     const data = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const Card = () => {
-    return <View style={styles.card} />;
+    const onPress = () => {
+        navigation.navigate('ProductDetailsScreen');
+    }
+    const Card = () => {
+        return <TouchableOpacity onPress={onPress} style={styles.card} />;
 };
     
     const renderItem = () => {
